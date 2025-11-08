64f8c6a0315a3218e1cf50a75f467c83b41278da
Zal76 <rizalbwg123@gmail.com>
2025-11-08 16:12:14 +0700

    Initial commit: upload portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+import DataImage, { listTools, listProyek} from "./data";
+import {} from "./data";
+
+function App() {
+  return (
+    <>
+     <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1 ">
+      <div className="animate__animated animate__fadeInUp animate__delay-3s">
+        <div className="flex items-center gap-3 mb-6 bg-zinc-800 p-4 rounded-2xl w-fit">
+      <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"  loading="lazy"/>
+      <p>Bisa Karena Terbiasa</p>
+      </div>
+      <h1 className="text-5xl/tight font-bold mb-6"> Hi!, Saya Muhammad Rizal Ramdhani</h1>
+      <p className="text-base/loose mb-6 opacity-80">Mahasiswa Program Studi Pendidikan Teknik Informatika semester 5 dengan pengalaman organisasi dan kepanitiaan. Memiliki minat besar pada pengembangan aplikasi berbasis web, baik front-end maupun back-end. Memiliki dasar pemahaman tentang HTML, CSS, JavaScript, dan PHP, serta pengetahuan dasar tentang database MySQL. Terbiasa bekerja sama dalam tim dan berkomitmen untuk terus mengasah kemampuan di bidang pengembangan aplikasi web.</p>
+      <div className="flex items-center sm:gap-4 gap-2">
+        <a href="#" className="bg-green-900 p-4 rounded-2xl hover:bg-green-700 mr-4">Download CV<i className="ri-download-line ri-lg"></i>
+        </a>
+        <a href="#proyek" className="bg-green-900 p-4 rounded-2xl hover:bg-green-700">View Project<i className="ri-arrow-down-line ri-lg"></i></a>
+      </div>
+      </div>
+      <img src={DataImage.HeroImage} alt="Hero Image"  className="w-[500px] md:ml-auto animate__animated animate__fadeInUp animate__delay-4s" loading="lazy"/>
+     </div>
+
+     {/* Section Tentang */}
+     <div className="tentang mt-32 py-10" id="tentang">
+      <div className="x1:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-1g" data-aos="fade-up" data-aos-duration="1000"data-aos-once="true">
+      <img src={DataImage.HeroImage} alt="Hero Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy"/>
+     <p className="text-base/loose mb-10">
+     Hi, Saya Memiliki minat besar pada pengembangan aplikasi berbasis web, baik front-end maupun back-end. Memiliki dasar pemahaman tentang HTML, CSS, JavaScript, dan PHP, serta pengetahuan dasar tentang database MySQL. Terbiasa bekerja sama dalam tim dan berkomitmen untuk terus mengasah kemampuan di bidang pengembangan aplikasi web.
+     </p>
+     <div className="flex items-center justify-between"> 
+    <img src={DataImage.HeroImage} alt="Hero Image" className="w-12 rounded-md sm:block hidden" loading="lazy"/>
+     <div className="flex items-center gap-6">
+     <div>
+      <div>
+        <h1 className="text-3xl mb-1">4<span className="text-green-800">+</span>
+        </h1>
+        <p>Proyek Selesai</p>
+      </div>
+        </div>
+      </div>
+      </div>
+      </div>
+
+      {/*Tools*/}
+     <div className="tools mt-32" id="tools">
+      <h1 className="text-4xl/snug font-bold mb-4" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Tools yang dipakai</h1>
+      <p className="flex" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+       Berikut tool yang biasa saya pakai untuk pengerjaan Project
+      </p>
+      <div className="tools-box mt-5 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+        {listTools.map(tool =>(
+      <div className="flex-items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}  
+      data-aos="fade-up" data-aos-duration="1000" data-aos-delay={tool.dad} data-aos-once="true">
+          <img src={tool.gambar}alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
+          <div> 
+            <h4 className="font-bold">{tool.nama}</h4>
+            <p className="opacity-50">{tool.ket}</p>
+          </div>
+        </div>
+        ))}
+      
+      </div>
+     </div>
+     </div>
+
+     {/* Proyek */}
+     <div className="proyek mt-32 py-10" id="proyek">
+  <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+  <p className="text-base/loose text-center opacity-50" 
+  data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+  Berikut ini beberapa proyek yang telah saya buat.</p>
+  <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2  grid-cols-1 gap-4">
+    {listProyek.map((proyek) => (
+      <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md"  
+      data-aos="fade-up" data-aos-duration="1000" data-aos-delay={proyek.dad} data-aos-once="true">
+        <img src={proyek.gambar} alt="Proyek Image" loading="lazy"/>
+        <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+        <p className="text-base/loose mb-4">{proyek.desk}</p>
+        <div className="flex flex-wrap gap-2">
+          {proyek.tools.map((tool, index) => (
+            <p className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold" key={index}>
+              {tool}
+            </p>
+          ))}
+        </div>
+      </div>
+    ))}
+    <div className="mt-8 text-center">
+      <a href="#" className="bg-green-800 p-3 rounded-lg block border border-zinc-600 hover:bg-green-700">
+        Lihat Website
+      </a>
+    </div>
+  </div>
+</div>
+
+{/* Kontak */}
+<div className="kontak mt-32 sm:p-10 p-0" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" data-aos-once="true" id="kontak">
+  <h1 className="text-4xl mb-2 font-bold text-center">
+    Kontak</h1>
+  <p className="text-base/loose text-center mb-10 opacity-50"  data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" data-aos-once="true"
+  >Mari terhubung dengan saya</p>
+  <form action="https://formsubmit.co/rizalbwg123@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off"
+   data-aos="fade-up" data-aos-duration="1000" data-aos-delay="700" data-aos-once="true">
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <label className="font-semibold">Nama Lengkap</label>
+        <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="font-semibold">Email</label>
+        <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="pesan" className="font-semibold">
+          Pesan
+        </label>
+        <textarea name="pesan" id="pesan" cols="45" rows="10" placeholder="Pesan... " className="border border-zinc-500 p-2 rounded-md">
+        </textarea>
+      </div>
+      <div className="text-center">
+        <button type="submit" className="bg-green-800 p-3 w-full cursor-pointer rounded-lg border border-zinc-600 hover:bg-green-700">Kirim Pesan</button>
+      </div>
+    </div>
+  </form>
+</div>
+    </>
+  );
+}
+
+export default App;
