8081521eac6f70e739f1afefc1df9dda661bc395
Zal76 <rizalbwg123@gmail.com>
2025-11-22 20:46:09 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
       {/* Hero Section */}
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div className="animate__animated animate__fadeInUp animate__delay-3s">
-          <div className="flex items-center gap-3 mb-6 bg-zinc-800 p-4 rounded-2xl w-fit"
-          id="tentang">
+          <div
+            className="flex items-center gap-3 mb-6 bg-zinc-800 p-4 rounded-2xl w-fit"
+            id="tentang"
+          >
             <img
               src={DataImage.HeroImage}
               alt="Hero Image"
